1996ac96357ad94030df002c3c09b27b80d5b042
Emi <47641194+Emilianoac@users.noreply.github.com>
2021-08-05 16:08:24 -0400

    Añadir Leaflet

--- src/components/Mapa.vue
+++ src/components/Mapa.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="mapa"></div>
+  <div class="mapa" ref="containerMapa"></div>
 </template>
 
 <script>
-export default {
-    name: "Mapa"
-}
+    import { ref, onMounted } from "vue"
+
+    export default {
+        setup() {
+            const containerMapa = ref(null)
+            const openImage = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            const openLink = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            let mapa
+
+            onMounted(() => {
+                mapa = L.map(containerMapa.value)
+                mapa.setView([51.505, -0.09], 13)
+                L.tileLayer(openImage , { attribution: openLink }).addTo(mapa)
+            })
+
+            return { containerMapa }
+        }
+    }
 </script>
 
 <style lang="scss">
--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import store from './store'
+import leaflef from 'leaflet/dist/leaflet'
+import 'leaflet/dist/leaflet.css'
 import './assets/scss/main.scss'
 
 let app = createApp(App)
 app.use(store)
+app.use(leaflef)
 app.mount('#app')
