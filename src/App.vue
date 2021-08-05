dda88faafce2486b1e0dbe15c38019a58e599c56
Emi <47641194+Emilianoac@users.noreply.github.com>
2021-08-05 15:16:51 -0400

    Crear componentes y estilos

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <h1>IP Adress Tracker</h1>
+  <div class="container">
+    <Banner/>
+    <Mapa/>
+  </div>
 </template>
 
 <script>
+  import Banner from "./components/Banner.vue"
+  import Mapa from "./components/Mapa.vue"
 
 export default {
   name: 'App',
   components: {
-
+    Banner,
+    Mapa
   }
 }
 </script>
 
 <style lang="scss">
-
 </style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,36 @@
+<template>
+    <header class="header"> 
+        <h1 class="header__titulo">IP Address Tracker</h1>
+        <Buscador/>
+        <Resultados/>
+    </header>
+</template>
+<script>
+import Buscador from "./Buscador.vue"
+import Resultados from "./Resultados.vue"
+
+export default {
+    components: {
+        Buscador,
+        Resultados
+    }
+}
+</script>
+
+<style lang="scss">
+
+    .header {
+        position: relative;
+        background-image: url("../../public/img/pattern-bg.png");
+        background-size: cover;
+        text-align: center;
+        color: white;
+        padding: 7em 1em;
+        padding-top: 4em;
+
+        .header__titulo {
+            margin-bottom: 0.8em;
+            font-weight: 500;
+        }
+    }
+</style>
