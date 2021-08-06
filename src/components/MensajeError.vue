1cf259c2aa6dbefa08f5ac19d2ba4df2356a308a
Emi <graficoemiliano@gmail.com>
2021-08-06 11:37:55 -0400

    Manejar busqueda invalida

---
+++ src/components/MensajeError.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="error">
+    <button @click="cerrarError" class="error__boton">x</button>
+     <p class="error__mensaje">{{mensaje}}</p> 
+  </div>
+</template>
+
+<script>
+    import {useStore} from "vuex"
+
+    export default {
+        name: "MensajeError",
+        props: ["mensaje"],
+        setup(props) {
+            const store = useStore()
+
+            return { 
+                cerrarError :() => store.commit('manejarError')
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+    .error {
+        display: block;
+        position: relative;
+        background-color: #ff0000bf;
+        max-width: 300px;
+        margin: auto;
+        padding: 0.4em;
+        border-radius: 4px;
+        position: absolute;
+        left: 50%;
+        width: 100%;
+        transform: translate(-50%);
+        top: 18px;
+        font-size: 0.9em;
+
+        .error__mensaje {
+            margin: 0;
+        }
+
+        .error__boton {
+            position: absolute;
+            top: 0;
+            right: 5px;
+            border: 0;
+            background-color: transparent;
+            color: white;
+            padding: 0;
+            cursor: pointer;
+        }
+    }
+
+</style>
