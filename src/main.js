f43eb8013aaba327ca2ad23fbc901eaf43fd3608
Emi <47641194+Emilianoac@users.noreply.github.com>
2021-08-06 10:20:13 -0400

    Actualizar mapa segun busqueda

--- src/main.js
+++ src/main.js
@@ -3,8 +3,18 @@ import App from './App.vue'
 import './registerServiceWorker'
 import store from './store'
 import leaflef from 'leaflet/dist/leaflet'
+import { Icon } from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import './assets/scss/main.scss'
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.imagePath = '/public/img/icon-location.svg';
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('/public/img/icon-location.svg'),
+  iconUrl: require('/public/img/icon-location.svg'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  iconSize: [30, 40]
+})
 
 let app = createApp(App)
 app.use(store)
